d6676f7402cd5976107a8c760effadb4e0598f3c
Huynhnhu07092001 <87075326+Huynhnhu07092001@users.noreply.github.com>
2021-07-07 19:50:22 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,17 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Anh iu ơiiiiii!',
+    introDesc: `Em iu của anh nò.
+    Huỳnh Như nè <3`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    title: 'Anh có iu em hongggg hả 😙',
+    desc: 'Anh có nhớ em hoonggggg?????? ',
+    btnYes: 'Có chớ siêu siêu nhiều <33',
+    btnNo: 'Không, ai gảnh như heooo',
+    question:'Thế anh có iu en honggg dạ <3',
+    btnReply: 'Gửi cho heo <3',
+    reply: 'Anh iu em nhiều lắm ó<333',
+    mess: 'Em nhớ anh lắm biết hông.I love u so muchhh 😘😘',
+    messDesc: 'Em muốn uống trà sữa osoooooo anh có thể ship cho em hông??.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/profile.php?id=100008590487937' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
